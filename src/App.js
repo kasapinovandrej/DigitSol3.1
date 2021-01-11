62e997dec78f992d48129e8b651817ad9da2469a
kasapinovandrej <kasapinovandrej@gmail.com>
2021-01-11 20:19:44 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './main.scss';
+import Home from './Pages/Home';
+import Portfolio from './Pages/Portfolio';
+import Contact from './Pages/Contact';
+import AboutUs from './Pages/About';
+import Header from './components/Header';
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>cao zdravo</h1>
+        <Header />
+        <Switch>
+          <Route path="/portfolio" exact component={Portfolio} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/about" component={AboutUs} />
+          <Route path="/" component={Home} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ImFacebook2, ImInstagram, ImLinkedin, ImPhone, ImMail3 } from 'react-icons/im';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header">
+                <div className="header__contact">
+                    <a className="header__link" href="tel:0038163-78-28-541"><ImPhone /><p>(00381)63-78-28-541</p></a>
+                    <a className="header__link" href="tel:0038169-44-89-990"><ImPhone /><p>(00381)69-44-89-990</p></a>
+                    <a className="header__link" href="mailto:office@digitalsolution.rs"><ImMail3 /><p>office@digitalsolution.rs</p></a>
+                </div>
+                <div className="header__contact header__social">
+                    <a className="header__link" href="https://www.facebook.com/digitalsolution.web"><ImFacebook2 className="header__icon" /></a>
+                    <a className="header__link" href="https://www.instagram.com/digitalsolution_web/?igshid=31pkfchkf6cd"><ImInstagram className="header__icon" /></a>
+                    <a className="header__link" href="https://www.linkedin.com/in/digital-solution-6581831bb/"><ImLinkedin className="header__icon" /></a>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
